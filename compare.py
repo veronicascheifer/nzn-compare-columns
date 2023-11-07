52fc01028cb5dfa55af35a0e48756167e79250db
Veronica <55854560+veronicascheifer@users.noreply.github.com>
2023-11-07 23:03:13 +0000

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,26 +1,26 @@
 import pandas as pd
 
-def comparacao(a, b):
+def comparison(a, b):
      if (a == b): 
-         return 'a linha E igual' 
+         return 'the row is equal' 
      else: 
-         return 'a linha NAO e igual'
+         return 'the row is not equal'
 
 
-df = pd.read_csv('arq.csv', ';', header=0, usecols=["Titulo", "titulo"])
+df = pd.read_csv('file.csv', ';', header=0, usecols=["Title", "title"])
 
-countTitulo1 = pd.DataFrame(df).count()[0]
-countTitulo2 = pd.DataFrame(df).count()[1]
+countTitle1 = pd.DataFrame(df).count()[0]
+countTitle2 = pd.DataFrame(df).count()[1]
 
-file = open("resultado_comparacao.txt", "w")
-file.write("textoTitulo;resultadoDaComparacao\n")  
+file = open("comparison_results.txt", "w")
+file.write("textTitle;comparisonResults\n")  
 
 string1 = df.iat[0,0]
 string2 = df.iat[0,1]
 
 
-for linha in range(countTitulo1):
-    retorno = comparacao(df.iat[linha,0], df.iat[linha,1])
-    file.write(df.iat[linha,0] + ";" + retorno + "\n")  
+for line in range(countTitle1):
+    return1 = comparison(df.iat[line,0], df.iat[line,1])
+    file.write(df.iat[line,0] + ";" + return1 + "\n")  
 
 file.close() 
